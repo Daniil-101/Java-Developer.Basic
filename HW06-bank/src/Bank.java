1ebdec95a74b4676bd8a2eb70625ab2cb82d6de4
Daniil-101 <kirden98@gmail.com>
2023-05-15 18:53:28 +0300

    HW06-working with bank customer accounts

--- HW06-bank/src/Bank.java
+++ HW06-bank/src/Bank.java
@@ -6,29 +6,29 @@ public class Bank {
     private final HashMap<Client, ArrayList<Account>> allCustomerAccounts = new HashMap<>();
     private final HashMap<Account, Client> findClient = new HashMap<>();
 
-    public void clientCard(Client client, ArrayList<Account> accounts){
-        allCustomerAccounts.put(client,accounts);
-        for(Account ac : accounts){
-            findClient.put(ac,client);
+    public void clientCard(Client client, ArrayList<Account> accounts) {
+        allCustomerAccounts.put(client, accounts);
+        for (Account ac : accounts) {
+            findClient.put(ac, client);
         }
     }
 
-    public Client customerSearch(Account account){
+    public Client customerSearch(Account account) {
         return findClient.get(account);
     }
 
-    public ArrayList<Account> searchAllAccounts(Client client){
+    public ArrayList<Account> searchAllAccounts(Client client) {
         return allCustomerAccounts.get(client);
     }
 
-    public void info(){
+    public void info() {
         Scanner sc = new Scanner(System.in);
         System.out.println("Введите имя клиента или номер счета: ");
-        if (sc.hasNextInt()){
+        if (sc.hasNextInt()) {
             int id = sc.nextInt();
             Account account = new Account(id);
             System.out.println(customerSearch(account));
-        }else {
+        } else {
             String name = sc.next();
             System.out.println("Введите возраст клиента: ");
             int age = sc.nextInt();
--- HW06-bank/src/Client.java
+++ HW06-bank/src/Client.java
@@ -20,7 +20,6 @@ public class Client {
     }
 
 
-
     @Override
     public String toString() {
         return "Client " + name + ", " + age + " years old";
